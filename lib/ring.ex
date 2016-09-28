fa3903391791b867a69330f4e7670a7a362ef473
Dave Lively <davelively14@gmail.com>
2016-09-28 11:16:32 -0400

    Added Ring

--- lib/ring.ex
+++ lib/ring.ex
@@ -1,2 +1,18 @@
 defmodule Ring do
+
+  # Create n number of processes that each run the loop function. Returns
+  # a list of spawned pids.
+  def create_processes(n) do
+    1..n |> Enum.map(fn _ -> spawn(fn -> loop end) end)
+  end
+
+  def loop do
+    receive do
+      {:link, link_to} when is_pid(link_to) ->
+        Process.link(link_to)
+        loop
+      :crash ->
+        1/0
+    end
+  end
 end
